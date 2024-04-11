292c218243d5362889926c2f24160bb4c00c53ad
PerniceSimone <simone.pernice@unito.it>
2024-04-11 16:21:34 +0200

    adding anayhting

--- Main.R
+++ Main.R
@@ -14,19 +14,27 @@ model_name = "iML1515"
 #Prendo il riferimento al percorso della rete (GreatMode)
 net_wd = paste0(wd, "/net")
 net_fname = paste0(model_name, "_PN")
+fba_fname = paste0(model_name, "_fba")
 
 print(net_fname)
 
+model = FBA4Greatmod.generation(
+  fba_mat = './input/iML1515.mat'
+)
 
-model = FBA4Greatmod.generation(fba_mat = './input/iML1515.mat',
-                        fba_model = './net/iML1515_PN.PNPRO'
-                        )
-
-writeFBAfile(model, './result/result')
+writeFBAfile(model, paste0('./input/',fba_fname) )
 
 #Prove generazione + analisi 
 
-model_gen = model.generation(net_fname = './net/iML1515_PN.PNPRO', transitions_fname = './net/transitions.cpp', fba_fname = './result/result')
+model_gen = model.generation(
+  net_fname = './net/iML1515_PN.PNPRO',
+  transitions_fname = './net/transitions.cpp',
+  fba_fname =  paste0('./input/',fba_fname)
+)
 
 
-model.analysis(solver_fname = 'iML1515_PN.solver', f_time = 10, s_time = 0.5)
+model.analysis(
+  solver_fname = 'net/iML1515_PN.solver',
+  f_time = 10, s_time = 0.5,
+  debug = T,
+  fba_fname =  paste0('./input/',fba_fname) )
